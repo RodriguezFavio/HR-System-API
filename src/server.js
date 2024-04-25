16511defd7be6d0511c553beabc898fc7186bea6
RodriguezFavio <favio.rodriguez.flores@gmail.com>
2024-04-25 15:58:30 -0300

    Add middleware for CORS handling and error management in API routes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,29 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const hrSystemRoutes = require('./routes/hrSystemRoutes');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use('/api', hrSystemRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message });
+  next();
+});
 
 app.listen(8080);
