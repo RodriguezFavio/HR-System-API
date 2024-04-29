cfcf3b89a0daa8feb2039421c56f7ee53876ac6a
RodriguezFavio <favio.rodriguez.flores@gmail.com>
2024-04-29 18:48:42 -0300

    add HrSystemApi class and get course,trainer and learners of the HR Api system

--- src/services/hrSystemService.js
+++ src/services/hrSystemService.js
@@ -1,9 +1,35 @@
 const axios = require('axios');
+const Course = require('../model/course');
+const Trainer = require('../model/trainer');
 
-const BASE_URL = 'http://127.0.0.1:3000';
+const BASE_URL = 'http://127.0.0.1:3000/api';
 
-exports.getTrainers = async () => {
-  const response = await axios.get(`${BASE_URL}/api/trainers`);
+class HrSystemApi {
+  static async getCourse(courseId) {
+    const response = await axios.get(`${BASE_URL}/courses/${courseId}`);
+    const { id, title, date, trainerId, learners } = response.data;
 
-  return response.data;
-};
+    const course = new Course(id, title, date, trainerId, learners);
+
+    return course;
+  }
+
+  static async getTrainer(trainerId) {
+    const response = await axios.get(`${BASE_URL}/trainers/${trainerId}`);
+    const { id, name } = response.data;
+
+    const trainer = new Trainer(id, name);
+
+    return trainer;
+  }
+
+  static async getLearners(ids) {
+    const response = await axios.get(`${BASE_URL}/learners`);
+    const { learners } = response.data;
+    const learnersList = learners.filter((learner) => ids.includes(learner.id));
+
+    return learnersList;
+  }
+}
+
+module.exports = HrSystemApi;
