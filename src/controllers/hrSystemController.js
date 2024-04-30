9f5486fcba4ee1a4817e7fa03b39b6ccf0b2382f
RodriguezFavio <favio.rodriguez.flores@gmail.com>
2024-04-30 11:25:41 -0300

    Replaced getTrainers function with getCourse, getTrainer, getLearners functions and added error handling for bad requests

--- src/controllers/hrSystemController.js
+++ src/controllers/hrSystemController.js
@@ -1,21 +1,29 @@
-const handleService = require('../services/hrSystemService');
+const HrSystemApi = require('../services/hrSystemService');
+const APIError = require('../middleware/error');
 
-exports.getTrainers = async (req, res, next) => {
+exports.getCourses = async (req, res, next) => {
   try {
-    const trainers = await handleService.getTrainers();
+    const courseId = req.params.id;
+    const course = await HrSystemApi.getCourse(courseId);
+    const trainer = await HrSystemApi.getTrainer(course.trainerId);
+    const learners = await HrSystemApi.getLearners(course.learners);
 
-    console.log(trainers);
-    if (trainers) {
-      res.status(200).json({ message: 'Trainers found', trainers });
-    } else {
-      const error = new Error('Trainers not found!');
-      error.statusCode = 404;
-      throw error;
-    }
+    const lepayaCourse = {
+      id: course.id,
+      title: course.title,
+      date: course.date,
+      trainer: {
+        id: trainer.id,
+        name: trainer.name,
+      },
+      learners: learners.map((learner) => ({
+        id: learner.id,
+        name: learner.name,
+      })),
+    };
+
+    res.status(200).json({ message: 'Course found', lepayaCourse });
   } catch (err) {
-    if (!err.statusCode) {
-      err.statusCode = 500;
-    }
-    next(err);
+    next(new APIError(400, 'Bad Request'));
   }
 };
