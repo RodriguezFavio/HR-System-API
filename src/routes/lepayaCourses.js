f0104f6d699e44afebdde7f9d118d27f8f218b67
RodriguezFavio <favio.rodriguez.flores@gmail.com>
2024-05-01 15:16:31 -0300

    Add content from the validation file and use it in the routing file

--- src/routes/lepayaCourses.js
+++ src/routes/lepayaCourses.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
+const systemController = require('../controllers/hrSystemController');
+const validation = require('../middleware/validation');
+
 const router = express.Router();
 
-const systemController = require('../controllers/hrSystemController');
-
-router.get('/lepaya-courses/:id', systemController.getCourses);
+router.get(
+  '/lepaya-courses/:id',
+  [validation.validationId, validation.handleValidationErrors],
+  systemController.getCourses
+);
 
 module.exports = router;
